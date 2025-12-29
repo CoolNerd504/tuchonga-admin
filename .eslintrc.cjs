58845b50c79375f048c89d29689dff99492d31b2
Francis Lombe <lombefrancis@gmail.com>
2025-12-29 15:11:04 +0200

    Disable return-await rule for JS lint override

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,6 +47,7 @@ module.exports = {
         '@typescript-eslint/dot-notation': 'off',
         '@typescript-eslint/no-implied-eval': 'off',
         '@typescript-eslint/no-throw-literal': 'off',
+        '@typescript-eslint/return-await': 'off',
       },
     },
   ],
